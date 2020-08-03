a81f163500210992e96427e5654b5ec022dc5acc
Shinnosuke Komatsu <komatsu-shinnosuke@ed.tmu.ac.jp>
2020-08-03 15:26:24 +0900

    グラフィックを縦に並べた

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 var count = 0;
+let N = 255;
+let L = 255;
 
 const windowSizeHight = window.innerHeight * 6;
 // document.getElementById('main').innerHTML = 
@@ -21,7 +23,7 @@ function draw() {
   translate(0,-windowSizeHight/2);
   background(0);
   var scrY = window.scrollY;
-  console.log(scrY);
+  //console.log(scrY);
   textSize(40);
   text(scrY);
 
@@ -29,15 +31,36 @@ function draw() {
   // noFill();
   // box(200,200,200);
 
-  strokeWeight(200);
-  point(0,0,0);
+  // stroke(255);
+  // strokeWeight(200);
+  // point(0,0,0);
+
+  if(scrY <= 500){
+    drawWave(-window.innerWidth/2,scrY + 500,scrY/10);
+  }else{
+    drawWave(-window.innerWidth/2,1000,scrY/10);
+  }
 
   // count  = scrY;
   // if(scrY <= 6000){
   //   drawTorus(0,500);
   // }
 
-  drawRose(0,scrY + 400,scrY/100,2);
+  if(scrY > 500 && scrY <= 1300){
+    drawRose(0,1700,scrY/100,2);
+  }
+  if(scrY > 1300 && scrY <= 2300){
+    drawRose(0,scrY + 400,scrY/100,2);
+  }else{
+    drawRose(0,2700,10,2);
+  }
+
+  //drawMandelbrot(0,0);
+
+  // strokeWeight(2);
+  // noFill();
+  // box(200,200,200);
+
   // strokeWeight(200);
   // stroke(255,0,0);
   // point(0,200,0);
@@ -111,6 +134,82 @@ function drawRose(_x,_y,n,d){
     x1 = x2;
     y1 = y2;
   }
+  pop();
+}
+
+function drawMandelbrot(_x,_y){
+
+  var SCALE = 3.0;
+  var _x = 0;
+  var count = 0;
+
+  push();
+  translate(_x,_y);
+  strokeWeight(1);
+  noFill();
+
+  for (let a = -window.innerWidth / 2; a <= window.innerWidth / 2; a++) {
+    for (let b = -window.innerHeight / 2; b <= -window.innerHeight / 2; b++) {
+      let x = SCALE * a / window.innerWidth;
+      let y = SCALE * b / window.innerHeight;
+      let r = calc(x, y);
+      stroke(r * 2,r * 2,r * 2);
+      rect(a, b, 1, 1);
+    }
+  }
+  pop();
+}
+
+function calc(x, y) {
+  let tx, ty;
+  let zx = 0.0;
+  let zy = 0.0;
+ 
+  for (let i = 1; i <= N; i++) {
+    tx = zx;
+    ty = zy;
+ 
+    zx = tx * tx - ty * ty + x;
+    zy = 2 * tx * ty + y;
+ 
+    if (zx * zx + zy * zy > L)
+      return i;
+  }
+  return 0;
+}
+
+function drawWave(_x,_y,theta){
+  var sx = 0;
+  var sy = 0;
+  var cx = 0;
+  var cy = 0;
+  var tx = 0;
+  var ty = 0;
+  //var theta = 0;
+  let a = 100;
+
+  push();
+  translate(_x,_y);
+
+  // theta += 0.1;
+  //cx += 2;
+  // sx += 2;
+  //console.log(theta);
+  cx = theta * 20;
+  sx = theta * 20;
+  tx = theta * 20;
+  cy = a * cos(theta);
+  sy = a * sin(theta);
+  ty = a * tan(theta);
+
+  strokeWeight(50);
+  stroke(255);
+  point(cx,cy);
+  point(sx,sy);
+  point(tx,ty);
+  strokeWeight(2);
+  line(0,0,window.innerWidth,0);
+
   pop();
 }
 
